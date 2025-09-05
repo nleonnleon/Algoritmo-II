8ac409c1b2ccf287f0ed3396a3b384b09ca66792
Norma <nleonnleon@gmail.com>
2025-09-04 22:57:21 -0500

    Add files via upload

--- arbolBase.py
+++ arbolBase.py
@@ -1,7 +1,11 @@
+# Ejemplo de implementación de un árbol genealógico en Python
+# Abol no binario, cada nodo puede tener múltiples hijos
+# Cada nodo representa a una persona con su nombre  
+#Es el constructor de la clase. Se ejecuta cuando se crea una nueva instancia del árbol
 class Arbol:
     def __init__(self, elemento):
-        self.hijos = []
-        self.elemento = elemento
+        self.hijos = [] #Inicializa una lista vacía que almacenará los nodos hijos del nodo actual. Esto permite que cada nodo tenga múltiples hijos.
+        self.elemento = elemento #Guarda el valor o contenido del nodo
 
 def agregarElemento(arbol, elemento, elementoPadre):
     subarbol = buscarSubarbol(arbol, elementoPadre);
@@ -16,6 +20,7 @@ def buscarSubarbol(arbol, elemento):
         if (arbolBuscado != None):
             return arbolBuscado
     return None
+# Ejemplo de uso
 abuela = "Inocenta Arce"
 hija1 = "Delia Lescano"
 hija2 = "Sontos Lescano"
@@ -27,6 +32,7 @@ nieto4 = "Rosario Sanchez"
 nieto5 = "Julio Givar"    
 nieto6 = "LuManuel Givar"    
 arbol = Arbol(abuela)
+
 agregarElemento(arbol, hija2, abuela)
 agregarElemento(arbol, hija3, abuela)
 agregarElemento(arbol, nieto4, hija2)
@@ -41,6 +47,8 @@ def imprimirArbol(arbol, nivel=0):
     print("  " * nivel + arbol.elemento)
     for hijo in arbol.hijos:
         imprimirArbol(hijo, nivel + 1)
+
+
 
 # Imprimir el árbol
 imprimirArbol(arbol)
